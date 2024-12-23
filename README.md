effe087378588947baf0bb87a022980bc2c06217
BackNNHH <128043133+BackNNHH@users.noreply.github.com>
2024-12-23 16:34:05 +0700

    Add SG69

---
+++ README.md
@@ -0,0 +1,2 @@
+# nothing here
+![icon](assets/imgs/63.png)
---
+++ scripts.js
@@ -0,0 +1,4 @@
+function toggleBackground() {
+  document.querySelector(".bg").classList.toggle("bg-hidden");
+  document.querySelector(".tbg").classList.toggle("bg-hidden");
+}
